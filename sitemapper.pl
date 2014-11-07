cebb7ad63344a7724d631a01e9231f9cd7bbaeef
Karen Cravens <silver@phoenyx.net>
2014-11-06 21:00:36 -0600

    Updated sitemapper to produce JSON

--- sitemapper.pl
+++ sitemapper.pl
@@ -6,6 +6,7 @@ use strict;
 use WWW::RobotRules;
 use LWP::Simple qw(get);
 use POSIX qw(strftime);
+use JSON::PP;
 
 my $rootdir = '/home/';
 my $rooturl = 'http://tilde.club/~';
@@ -20,6 +21,7 @@ my $map = {};
 my $updates = {};
 my $byuser = '/home/silver/public_html/sitemap.html';
 my $bypage = '/home/silver/public_html/sitemap2.html';
+my $byjson = '/home/silver/public_html/sitemap.json';
 
 open my $fh, '<', $killfile || die;
 while (<$fh>) {
@@ -47,26 +49,27 @@ close $fh;
 
 opendir(my $dh, $rootdir) || die;
 while (readdir $dh) {
-	my $username = $_;
+        my $username = $_;
 	$updates->{$username} = 0;
 	if ($ignores->{$username}) {
-		$map->{$rooturl . $username . '/index.html'} = {
-			file => $rootdir . $username . $webdir . '/index.html',
-			username => $username,
-			name => "Killfiled",
-			private => 1,
-			mtime => 0,
-		};
+#		$map->{$rooturl . $username . '/index.html'} = {
+#			file => $rootdir . $username . $webdir . '/index.html', 
+#			username => $username,
+#			name => "Killfiled", 
+#			private => 1,
+#			mtime => 0,
+#		};
 		next;
 	}
-	if (-d $rootdir . $username . $webdir) {
-		checkdir ($username, q{});
-	}
+        if (-d $rootdir . $username . $webdir) {
+                checkdir ($username, q{});
+        }
 }
 closedir $dh;
 
 open my $bph, '>', $bypage;
 open my $buh, '>', $byuser;
+
 print $bph '<!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="en"><head><title>Tildemap (date)</title></head><body>', "\n";
 print $buh '<!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="en"><head><title>Tildemap (user)</title></head><body>', "\n";
 print $bph '<h1>Sitemap</h1>', "\n";
@@ -94,7 +97,7 @@ foreach my $uri (sort {$map->{$b}->{mtime} <=> $map->{$a}->{mtime}} keys $map) {
 		$date = strftime("%d-%b-%Y", gmtime($map->{$uri}->{mtime}));
 		print $bph "<h3>$date</h3>\n";
 	}
-	print $bph '<p><a href="http://tilde.club/~', $map->{$uri}->{'username'}, '/">', $map->{$uri}->{'username'}, '</a>: <a href="' , $uri, '">', $map->{$uri}->{n$
+	print $bph '<p><a href="http://tilde.club/~', $map->{$uri}->{'username'}, '/">', $map->{$uri}->{'username'}, '</a>: <a href="' , $uri, '">', $map->{$uri}->{name}, '</a></p>', "\n";
 }
 print $buh '<hr><p>Respects killfile at /home/brendn/bin/botify/killfile and robots.txt in each directory.</p>', "\n";
 print $buh '<p>Directories can be chmod o-r to prevent local browsing (including this script) but still allow web serving.</p>', "\n";
@@ -105,18 +108,25 @@ print $bph '<p>Directories can be chmod o-r to prevent local browsing (including
 print $bph '<p>Last updated: ', strftime("%d-%b-%Y %H:%M", gmtime()), "</p>\n";
 print $bph '</body></html>', "\n";
 
+close $buh;
+close $bph;
+
+open my $bjh, '>', $byjson;
+print $bjh encode_json $map;
+close $bjh;
+
 sub checkdir {
-	my $username = shift;
-	my $dircheck = shift;
+        my $username = shift;
+        my $dircheck = shift;
 
 	if (!-r $rootdir . $username . $webdir . $dircheck) {
-		$map->{$rooturl . $username . $dircheck . '/index.html'} = {
-			file => $rootdir . $username . $webdir . '/index.html',
-			username => $username,
-			name => "Directory not publicly readable",
-			private => 1,
-			mtime => 0,
-		};
+#		$map->{$rooturl . $username . $dircheck . '/index.html'} = {
+#			file => $rootdir . $username . $webdir . '/index.html', 
+#			username => $username,
+#			name => "Directory not publicly readable", 
+#			private => 1,
+#			mtime => 0,
+#		};
 		return;
 	}
 	{
@@ -124,20 +134,20 @@ sub checkdir {
 		my $robots_txt = get $url;
 		$rules->parse($url, $robots_txt) if defined $robots_txt;
 	}
-	opendir(my $wdh, $rootdir . $username . $webdir . $dircheck) || die $rootdir . $username . $webdir . $dircheck . ': ' . $!;
-	while (readdir $wdh) {
-		my $filename = $_;
-		next if $filename =~ /^\./; # hidden
+        opendir(my $wdh, $rootdir . $username . $webdir . $dircheck) || die $rootdir . $username . $webdir . $dircheck . ': ' . $!;
+        while (readdir $wdh) {
+                my $filename = $_;
+                next if $filename =~ /^\./; # hidden
 		my $fullfile = $rootdir . $username . $webdir . $dircheck . '/' . $filename;
-		next if (exists $denies->{$fullfile});
-		next if (exists $denies->{$rootdir . $username . $webdir . $dircheck} && !exists $allows->{$fullfile});
+		next if (exists $denies->{$fullfile}); 
+		next if (exists $denies->{$rootdir . $username . $webdir . $dircheck} && !exists $allows->{$fullfile}); 
 		next if (-l $fullfile); # symbolic
 		next if !$rules->allowed("$rooturl$username$dircheck/$filename");
-		if (-d $fullfile) {
-			checkdir ($username, $dircheck . '/' . $filename);
-			next;
-		}
-		next if $filename !~ /^(.*?)\.html$/; # not a vanilla page
+                if (-d $fullfile) {
+                        checkdir ($username, $dircheck . '/' . $filename);
+                        next;
+                }
+                next if $filename !~ /^(.*?)\.html$/; # not a vanilla page
 		my $name = $dircheck . ' ' . $1;
 		if ($name eq ' index') {
 			$name = '<b>Home Page</b>';
@@ -149,11 +159,12 @@ sub checkdir {
 		$map->{"$rooturl$username$dircheck/$filename"} = {
 			file => $fullfile,
 			username => $username,
-			name => $name,
+			name => $name, 
 			ctime => $ctime,
 			mtime => $mtime,
 			size => $size,
 			private => 0,
 		};
-	}
+        }
 }
+
